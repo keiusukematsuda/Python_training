172bed20ad32e3366aecd917a5d454086ec96012
root <root@ip-10-0-0-143.ap-northeast-1.compute.internal>
2019-01-06 01:47:48 +0000

    add functions

--- pic_carver.py
+++ pic_carver.py
@@ -10,6 +10,49 @@ pictures_directory = "pictures"
 faces_directory = "faces"
 pcap_file = "bhp.pcap"
 
+def get_http_header(http_payload):
+    try:
+        # HTTPヘッダーを抽出
+        headers_raw = http_payload[:http_payload.index("\r\n\r\n")+2]
+        
+        # ヘッダーの各要素を辞書化
+        headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n",headers_raw))
+
+    except:
+        return None
+
+    if "Content-Type" not in headers:
+        return None
+
+    return headers
+
+
+def extract_image(headers,http_payload):
+    image       = None
+    image_type  = None
+
+    try:
+        if "image" in headers['Content-Type']:
+            
+            # 画像種別と画像本体の取得
+            image_type = headers['Content-Type'].split("/")[1]
+
+            image = http_payload[http_payload.index("\r\n\r\n")+4:]
+
+            # 画像が圧縮されている場合は解凍
+            try:
+                if "Content-Encoding" in headers.keys():
+                    if headers['Content-Encoding'] == "gzip":
+                        image = zlib.decompress(image, 16+zlib.MAX_WBITS)
+                    elif header['Content-Encoding'] == "deflate":
+                        image = zlib.decompress(image)
+
+            except:
+                return None,None
+
+            return image,image_type
+
+
 def http_assembler(pcap_file):
     carved_images = 0
     faces_detected = 0
